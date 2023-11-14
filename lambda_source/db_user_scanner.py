cc7cacfe9398b139aac19e29f87b4c00e1971d19
Leon Jiang <leon321jiang@users.noreply.github.com>
2023-11-13 22:02:30 -0800

    refactor db helper

--- lambda_source/db_user_scanner.py
+++ lambda_source/db_user_scanner.py
@@ -7,14 +7,13 @@ from .dynamo_ops import update_users_roles_records
 dynamodb = boto3.resource('dynamodb')
 
 db_users_roles_table_name = 'db_users_roles'
-sqs_queue_url = 'https://sqs.us-west-2.amazonaws.com/202053868822/db-scan-notification'
-onboarded_db_list = 'onboarded_db_list'
+onboarded_db_table_name = 'onboarded_db_list'
 
 def db_user_scanner(event, context):
     # Read the list of databases from the 'onboarded_db_list' DynamoDB table
-    onboarded_db_list_table = dynamodb.Table(onboarded_db_list)
+    onboarded_db_table = dynamodb.Table(onboarded_db_table_name)
     try:
-        onboarded_dbs = onboarded_db_list_table.scan().get('Items', [])
+        onboarded_dbs = onboarded_db_table.scan().get('Items', [])
     except ClientError as e:
         print(e.response['Error']['Message'])
         return {'statusCode': 500, 'body': e.response['Error']['Message']}
